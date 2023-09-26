8765fa5f4b2e75ad1e061590e16499c1d51c0d72
jeffersonmatias <138624962+jeffersonmatias@users.noreply.github.com>
2023-09-26 15:09:42 -0300

    added responsivation and styles

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -4,7 +4,7 @@ import { dataFake } from '../../data/dataFake'
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
-  styleUrls: ['./content.component.css']
+  styleUrls: ['./content.component.css','./content.responsive.component.css']
 })
 export class ContentComponent implements OnInit {
 
@@ -12,6 +12,8 @@ export class ContentComponent implements OnInit {
   photoCover: string = ""
   contentTitle: string = ""
   contentDescription: string = ""
+  link01: string = ""
+  link02: string = ""
   private id: string | null = "0"
 
   constructor(
@@ -19,20 +21,23 @@ export class ContentComponent implements OnInit {
   ) { }
 
   ngOnInit(): void {
-    this.route.paramMap.subscribe( value =>
+    this.route.paramMap.subscribe(value =>
       this.id = value.get("id")
     )
 
     this.setValuesToComponent(this.id)
   }
 
-  setValuesToComponent(id:string | null){
+  setValuesToComponent(id: string | null) {
     const result = dataFake.filter(article => article.id == id)[0]
 
 
-   this.contentTitle = result.title
-   this.contentDescription = result.description
-   this.photoCover = result.photoCover
+    this.contentTitle = result.title
+    this.contentDescription = result.description
+    this.photoCover = result.photoCover
+    this.link01 = result.link01
+    this.link02 = result.link02
+
 
   }
 }
