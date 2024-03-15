3e4e72796a23a9b34dc8a3ca58088d4baa1d18b1
jeffersonmatias <138624962+jeffersonmatias@users.noreply.github.com>
2024-03-15 17:56:04 -0300

    new images

--- src/app/data/dataFake.ts
+++ src/app/data/dataFake.ts
@@ -3,24 +3,24 @@ export const dataFake = [
         "id": "1",
         "title": "Corinthians acumula mais de R$ 5 milhões em premiação com classificação na Copa do Brasil",
         "description": "Timão vai para a terceira fase do torneio depois de bater o São Bernardo por 2 a 0",
-        "photoCover": "https://abre.ai/jdTK",
+        "photoCover": "https://i.imgur.com/ttCW5Td.png",
     },
     {
         "id": "2",
         "title": "Brasileirão Feminino 2024: conheça os times, formato, duração e premiação",
         "description": "Campeonato começa nesta sexta, com 16 times; Fluminense, Botafogo, América-MG e Bragantino são as novidades",
-        "photoCover": "https://abre.ai/jdUj",
+        "photoCover": "https://i.imgur.com/joVFYtR.jpg",
     },
     {
         "id": "3",
         "title": "DUELO DE PESO",
         "description": "Quartas de final da Liga dos Campeões terá reencontro entre Real Madrid e Manchester City; veja duelos, datas e chaveamento até a decisão",
-        "photoCover": "https://abre.ai/jdUT",
+        "photoCover": "https://i.imgur.com/gMVr1yB.jpg",
     },
     {
         "id": "4",
         "title": "Pedro tenta marcar pelo sétimo jogo seguido",
         "description": "Atacante conseguiu gols em seis partidas seguidas no ano de 2023 e uma quina em 2020, sua primeira temporada no Rubro-Negro",
-        "photoCover": "https://abre.ai/jdU4",
+        "photoCover": "https://i.imgur.com/3hBgHgu.jpg",
     }
 ];
